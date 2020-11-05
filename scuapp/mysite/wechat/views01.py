1ad56272049ec771dc867ce16ec8cba94f1ef672
huhanlin <1351806208@qq.com>
2020-11-06 00:27:24 +0800

    删除稿2

--- scuapp/mysite/wechat/views01.py
+++ scuapp/mysite/wechat/views01.py
@@ -86,5 +86,6 @@ def management_inWork_reset(request):
 def management_inWork_delete(request):
     delete_num = request.GET.get('delete_num')
     TbinWork.objects.filter(iw_number=delete_num).delete()  # 批量删除
+    inwork_list = TbinWork.objects.all()
     return render(request, 'wechat/inwork_list.html', {'inwork_list': inwork_list})
 
