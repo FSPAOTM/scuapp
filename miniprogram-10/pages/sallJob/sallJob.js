46e02a3a9b78f3c3d4878a16930fc47febc9f862
tanld33 <tanld33@foxmail.com>
2020-11-18 16:33:04 +0800

    学生界面

--- miniprogram-10/pages/sallJob/sallJob.js
+++ miniprogram-10/pages/sallJob/sallJob.js
@@ -65,31 +65,113 @@ Page({
         'title': '文案编辑'
       }
     ],
-
+    areaList:[],
     pinpai_id: 0, //类型
     pinpai_txt: '',
     paixu_id: 0, //排序
     paixu_txt: '',
-    xiaoliang_id: 0, //筛选
-    xiaoliang_txt: '',
+    area_id: 0, //筛选
+    area_txt: '',
 
-    iworkinfo: [],
-    oworkinfo: [],
+    workinfo: [],
   },
 
   // 选项卡
   filterTab: function (e) {
+    var self = this;
     var data = [true, true, true],
       index = e.currentTarget.dataset.index;
     data[index] = !this.data.tab[index];
     this.setData({
       tab: data
     })
+    /*wx.request({
+      url: 'https://lbs.amap.com/api/webservice/guide/api/district',
+      method: "GET",
+      header: {
+        'Content-Type': 'application/json'
+      },
+      data:{
+        key:"80d1a82ecf4305a0cdd243289f293711",
+        keywords:"510100",
+        subdistrict:"1",
+        extensions:"base",
+        filter:"510100",
+      },
+      success: function (res) {
+        console.log(res);
+        if (res.status == 1) {
+          self.setData({
+            areaList: res.data.districts.district
+          })
+        }
+      }
+    })*/
   },
 
 
-  //筛选项点击操作
-  filter: function (e) {
+  //筛选项点击操作//待完善
+  filter1: function (e) {
+    var self = this,
+      id = e.currentTarget.dataset.id,
+      txt = e.currentTarget.dataset.txt,
+      tabTxt = this.data.tabTxt;
+    switch (e.currentTarget.dataset.index) {
+      case '0':
+        tabTxt[0] = txt;
+        self.setData({
+          tab: [true, true, true],
+          tabTxt: tabTxt,
+          leixing_id: id,
+          leixing_txt: txt
+        });
+        break;
+      case '1':
+        tabTxt[1] = txt;
+        self.setData({
+          tab: [true, true, true],
+          tabTxt: tabTxt,
+          paixu_id: id,
+          paixu_txt: txt
+        });
+        break;
+
+    }
+    //数据筛选
+    self.getDataList();
+  },
+
+  filter2: function (e) {
+    var self = this,
+      id = e.currentTarget.dataset.id,
+      txt = e.currentTarget.dataset.txt,
+      tabTxt = this.data.tabTxt;
+    switch (e.currentTarget.dataset.index) {
+      case '0':
+        tabTxt[0] = txt;
+        self.setData({
+          tab: [true, true, true],
+          tabTxt: tabTxt,
+          leixing_id: id,
+          leixing_txt: txt
+        });
+        break;
+      case '1':
+        tabTxt[1] = txt;
+        self.setData({
+          tab: [true, true, true],
+          tabTxt: tabTxt,
+          paixu_id: id,
+          paixu_txt: txt
+        });
+        break;
+
+    }
+    //数据筛选
+    self.getDataList();
+  },
+
+  filter3: function (e) {
     var self = this,
       id = e.currentTarget.dataset.id,
       txt = e.currentTarget.dataset.txt,
@@ -130,7 +212,30 @@ Page({
   onLoad: function (options) {
     let self = this;
     wx.request({
+      url: 'https://lbs.amap.com/api/webservice/guide/api/district',
+      method: "GET",
+      header: {
+        'Content-Type': 'application/json'
+      },
+      data:{
+        key:"80d1a82ecf4305a0cdd243289f293711",
+        keywords:"510100",
+        subdistrict:"1",
+        extensions:"base",
+        filter:"510100",
+      },
+      success: function (res) {
+        console.log(res);
+        if (res.status == 1) {
+          self.setData({
+            areaList: res.data.districts.district
+          })
+        }
+      }
+    })
+    wx.request({
       url: app.globalData.url + '/Show_inwork/',
+      /*待修改*/
       method: "GET",
       header: {
         'Content-Type': 'application/json'
@@ -139,13 +244,13 @@ Page({
         console.log(res);
         if (res.statusCode == 200) {
           self.setData({
-            iworkinfo: res.data
+            workinfo: res.data
           })
         }
       }
     })
 
-    wx.request({
+    /*wx.request({
       url: app.globalData.url + '/Show_outwork/',
       method: "GET",
       header: {
@@ -160,21 +265,31 @@ Page({
         }
       }
     
-    })
+    })*/
   },
 
   sijobinfo: function (ev) {
     var that = this;
     var e = ev.currentTarget.dataset.index;
-    var iw_number=that.data.iworkinfo[e].iw_number;
-    var type=that.data.iworkinfo[e].type;
-    console.log("++++++", ev, that)
-    wx.setStorageSync("iw_number", iw_number),wx.setStorageSync('type', type) ,wx.navigateTo({
-      url: "../sbaoming/sbaoming"
-    })
+    var type = that.data.workinfo[e].type;
+    if (that.data.workinfo[e].iw_number != "null") {
+      var iw_number = that.data.workinfo[e].iw_number;
+      console.log("++++++", ev, that)
+      wx.setStorageSync("iw_number", iw_number), wx.setStorageSync('type', type), wx.navigateTo({
+        url: "../sbaoming/sbaoming"
+      })
+    } else {
+      var ow_number = that.data.workinfo[e].ow_number;
+      console.log("++++++", ev, that)
+      wx.setStorageSync("ow_number", ow_number), wx.setStorageSync('type', type), wx.navigateTo({
+        url: "../sbaoming/sbaoming"
+      })
+    }
+
+
   },
 
-  sojobinfo: function (ev) {
+  /*sojobinfo: function (ev) {
     var that = this;
     var e = ev.currentTarget.dataset.index;
     var ow_number=that.data.oworkinfo[e].ow_number;
@@ -183,7 +298,7 @@ Page({
     wx.setStorageSync("ow_number", ow_number),wx.setStorageSync('type', type), wx.navigateTo({
       url: "../sbaoming/sbaoming"
     })
-  },
+  },*/
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -220,7 +335,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    app.editTabBar();  
+    app.editTabBar();
   },
 
   /**
@@ -237,11 +352,47 @@ Page({
 
   },
 
+  onRefresh() {
+    //在当前页面显示导航条加载动画
+    wx.showNavigationBarLoading();
+    //显示 loading 提示框。需主动调用 wx.hideLoading 才能关闭提示框
+    wx.showLoading({
+      title: '刷新中...',
+    })
+    this.getData();
+  },
+  //网络请求，获取数据
+  getData() {
+    let self = this;
+    wx.request({
+      url: app.globalData.url + '/Show_inwork/',
+      /*待修改*/
+      method: "GET",
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res);
+        if (res.statusCode == 200) {
+          self.setData({
+            workinfo: res.data
+          })
+        };
+        //隐藏loading 提示框
+        wx.hideLoading();
+        //隐藏导航条加载动画
+        wx.hideNavigationBarLoading();
+        //停止下拉刷新
+        wx.stopPullDownRefresh();
+      }
+    })
+  },
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    //调用刷新时将执行的方法
+    this.onRefresh();
   },
 
   /**
--- miniprogram-10/pages/smyJob/smyJob.js
+++ miniprogram-10/pages/smyJob/smyJob.js
@@ -14,7 +14,11 @@ Page({
         depart:"信息管理中心",
       }
     ],*/
-    workinfo:[],
+    workinfo1: [],
+    workinfo2: [],
+    workinfo3: [],
+    workinfo4: [],
+    workinfo5: [],
     //tap切换自定义宽高
     winWidth: 0,
     winHeight: 0,
@@ -74,20 +78,38 @@ Page({
       header: {
         'Content-Type': 'application/json'
       },
-      method:"GET",
-      data:{
-        user:app.globalData.user
+      method: "GET",
+      data: {
+        user: app.globalData.user
       },
       success: function (res) {
         console.log(res);
         if (res.statusCode == 200) {
-          self.setData({
-            workinfo: res.data
-          })
+          if (res.data.status == "") /*待修改*/ {
+            self.setData({
+              workinfo1: res.data
+            })
+          } else if (res.data.status == "") /*待修改*/ {
+            self.setData({
+              workinfo2: res.data
+            })
+          } else if (res.data.status == "") /*待修改*/ {
+            self.setData({
+              workinfo3: res.data
+            })
+          } else if (res.data.status == "") /*待修改*/ {
+            self.setData({
+              workinfo4: res.data
+            })
+          } else if (res.data.status == "") /*待修改*/ {
+            self.setData({
+              workinfo5: res.data
+            })
+          }
         }
       }
     });
-     /*wx.request({
+    /*wx.request({
       url: app.globalData.url + '/Show_myojob/',
       header: {
         'Content-Type': 'application/json'
@@ -119,7 +141,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    app.editTabBar();  
+    app.editTabBar();
   },
 
   /**
@@ -136,11 +158,66 @@ Page({
 
   },
 
+  onRefresh() {
+    //在当前页面显示导航条加载动画
+    wx.showNavigationBarLoading();
+    //显示 loading 提示框。需主动调用 wx.hideLoading 才能关闭提示框
+    wx.showLoading({
+      title: '刷新中...',
+    })
+    this.getData();
+  },
+  //网络请求，获取数据
+  getData() {
+    let self = this;
+    wx.request({
+      url: app.globalData.url + '/Show_inwork/',
+      /*待修改*/
+      method: "GET",
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res);
+        if (res.statusCode == 200) {
+          if (res.data.status == "") /*待修改*/ {
+            self.setData({
+              workinfo1: res.data
+            })
+          } else if (res.data.status == "") /*待修改*/ {
+            self.setData({
+              workinfo2: res.data
+            })
+          } else if (res.data.status == "") /*待修改*/ {
+            self.setData({
+              workinfo3: res.data
+            })
+          } else if (res.data.status == "") /*待修改*/ {
+            self.setData({
+              workinfo4: res.data
+            })
+          } else if (res.data.status == "") /*待修改*/ {
+            self.setData({
+              workinfo5: res.data
+            })
+          }
+        }
+        //隐藏loading 提示框
+        wx.hideLoading();
+        //隐藏导航条加载动画
+        wx.hideNavigationBarLoading();
+        //停止下拉刷新
+        wx.stopPullDownRefresh();
+      }
+    })
+  },
+
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    //调用刷新时将执行的方法
+    this.onRefresh();
   },
 
   /**
