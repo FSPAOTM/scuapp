57cdb195e7adeac01f711e88fa21f3c2d53762f6
Guo_qr <786958906@qq.com>
2020-12-21 00:48:44 +0800

    ,,,

---
+++ miniprogram-com/pages/sxiaoxi/sxiaoxi.js
@@ -0,0 +1,128 @@
+// pages/sxiaoxi/sxiaoxi.js
+const app=getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    name: '',
+    list: [{
+      id: "001",
+      name: "cc先生",
+      count: "2",
+      avatar: "../../images/head.png",
+      text: "正在载入聊天列表，请稍候",
+      updated: "2020.11.02"
+    }, {
+      id: "002",
+      name: "show",
+      count: "2",
+      avatar: "../../images/head.png",
+      text: "正在载入聊天列表，请稍候",
+      updated: new Date()
+    }, {
+      id: "003",
+      name: "as先生",
+      count: "2",
+      avatar: "../../images/head.png",
+      text: "正在载入聊天列表，请稍候",
+      updated: new Date()
+    }, {
+      id: "004",
+      name: "练习生",
+      count: "0",
+      avatar: "../../images/head.png",
+      text: "邮票吗",
+      updated: new Date()
+    }, {
+      id: "005",
+      name: "练习生o",
+      count: "2",
+      avatar: "../../images/head.png",
+      text: "正在载入聊天列表，请稍候",
+      updated: new Date()
+    }],
+   
+  },
+
+  mianshi() {
+    wx.navigateTo({
+      url: '../smianshitongzhi/smianshitongzhi?user=' + app.globalData.user,
+    })
+  },
+
+  jieguo() {
+    wx.navigateTo({
+      url: '../sjieguotongzhi/sjieguotongzhi?user=' + app.globalData.user,
+    })
+  },
+  goPage(e) {
+    let newlist = this.data.list
+    const index = e.currentTarget.dataset.index
+    newlist[index].count = 0;
+    this.setData({
+      list: newlist
+    })
+    wx.navigateTo({
+      url: '../schatting/schatting?name=' + e.currentTarget.dataset.name  /**?? */
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      name: app.globalData.user
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ miniprogram-stu/pages/schatting/schatting.js
@@ -0,0 +1,154 @@
+// pages/contact/contact.js
+const app = getApp();
+
+var inputVal = '';
+var msgList = [];
+var windowWidth = wx.getSystemInfoSync().windowWidth;
+var windowHeight = wx.getSystemInfoSync().windowHeight;
+var keyHeight = 0;
+
+/**
+ * 初始化数据
+ */
+// function initData(that) {
+//   inputVal = '';
+
+
+//   that.setData({
+//     msgList,
+//     inputVal
+//   })
+// }
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    scrollHeight: '100vh',
+    inputBottom: 0,
+    to: '',
+    name: '',
+    msgList: '',
+    inputVal: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      to: options.name,
+      name: app.globalData.user,
+      msgList: app.globalData.msgList,
+    }); 
+    this.receivemsg();
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 获取聚焦
+   */
+  focus: function (e) {
+    keyHeight = e.detail.height;
+    this.setData({
+      scrollHeight: (windowHeight - keyHeight) + 'px'
+    });
+    this.setData({
+      toView: 'msg-' + (msgList.length - 1),
+      inputBottom: keyHeight + 'px'
+    })
+    //计算msg高度
+    // calScrollHeight(this, keyHeight);
+
+  },
+
+  //失去聚焦(软键盘消失)
+  blur: function (e) {
+    this.setData({
+      scrollHeight: '100vh',
+      inputBottom: 0
+    })
+    this.setData({
+      toView: 'msg-' + (msgList.length - 1)
+    })
+
+  },
+
+  /**
+   * 发送点击监听
+   */
+  sendClick: function (e) {
+    var message = {}; //一条消息
+    message["from"] = this.data.name;
+    message["to"] = this.data.to;
+    message["time"] = Date.now();
+    message["content"] = e.detail.value;
+    message['isread'] = 1;
+
+    //发送消息
+    wx.sendSocketMessage({
+      data: JSON.stringify(message),
+      complete: this.updatemsglist(message),
+    })
+  },
+
+  updatemsglist: function (message) {
+    console.log("我在updatemsglist");
+    this.data.msgList.push(message);
+    inputVal = '';
+    this.setData({ //更新消息列表
+      inputVal,
+      msgList: this.data.msgList
+    });
+  },
+
+  receivemsg: function () {
+    console.log("receivemsg");
+    wx.showToast({
+      title: '收到一条新消息',
+    })
+    //接收消息后，添加到消息列表
+    wx.onSocketMessage((result) => {
+      var that = this;
+      let msg = JSON.parse(result.data)["message"];
+      for (var i = 0; i < msg.length; i++) {
+        that.data.msgList.push(msg[i]);
+      }
+      console.log(that.data.msgList)
+      this.setData({ //更新消息列表
+        msgList: that.data.msgList
+      });
+    })
+  },
+
+  /**
+   * 退回上一页
+   */
+  toBackClick: function () {
+    wx.navigateBack({})
+  }
+
+})
