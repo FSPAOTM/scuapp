ede744e08810f344714297ad09b68afedcbb3d79
tanld33 <tanld33@foxmail.com>
2021-01-04 01:33:05 +0800

    myjob深入界面

---
+++ miniprogram-stu/pages/smianshixinxi/smianshixinxi.js
@@ -0,0 +1,95 @@
+// pages/smianshixinxi/smianshixinxi.js
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    isShow: false,
+    ow_number:"",
+    mianshixinxi: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let that = this;
+    this.setData({
+      ow_number:options.ow_number
+    })
+    wx.request({
+      url: app.globalData.url + '/Stu_interview_notice_show/', //待修改
+      method: "GET",
+      header: {
+        'Content-Type': 'application/json'
+      },
+      data: {
+        ow_number:this.data.ow_number
+      },
+      success: function (res) {
+        console.log(res);
+        if (res.statusCode == 200) {
+          that.setData({
+            mianshixinxi: res.data
+          })
+        }
+        if (that.data.mianshixinxi.length == 0) {
+          that.setData({
+            isShow: true
+          })
+        }
+      }
+    })
+  },
+
+ 
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
